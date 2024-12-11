5915e048dc1513eb002edf171ca3bdf0aff3d2c2
Obsidian811 <156951145+Obsidian811@users.noreply.github.com>
2024-12-11 21:07:58 +0530

    commit

--- src/LandingPage/LandingPage.tsx
+++ src/LandingPage/LandingPage.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
+import styles from './LandingPage.module.css'
 
 const LandingPage: React.FC = () => {
     return (
-        <div style={landingStyle}>
+        <div className={styles.style}>
             <h1>Welcome to Our Website</h1>
             <p>Your journey starts here!</p>
         </div>
     );
 };
 
-const landingStyle: React.CSSProperties = {
-    backgroundImage: 'url(../../assets/Sus.gif)',
-    width: '100vw',
-    height: '100vh',
-    backgroundSize: 'cover',
-    color: 'white',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    textAlign: 'center',
-};
 
 export default LandingPage;
